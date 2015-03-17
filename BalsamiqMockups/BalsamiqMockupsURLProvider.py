a04f346bb4f536fdfaccccbb3e003b8a9604c03a
Nick Gamewell <rangergreen235@gmail.com>
2015-03-16 22:44:19 -0500

    Fixed balsamiq mockups recipe

---
+++ BalsamiqMockups/BalsamiqMockupsURLProvider.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#
+# Copyright 2014 Nick Gamewell
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+import urllib2, json
+
+from autopkglib import Processor, ProcessorError
+
+
+__all__ = ["BalsamiqMockupsURLProvider"]
+
+
+BASE_URL = "https://s3.amazonaws.com/build_production/mockups-desktop/version.jsonp"
+
+
+class BalsamiqMockupsURLProvider(Processor):
+#class BalsamiqMockupsURLProvider():
+    """Provides a download URL for the latest GoToMeeting release."""
+    input_variables = {
+        "base_url": {
+            "required": False,
+            "description": "Default is %s" % BASE_URL,
+        },
+    }
+    output_variables = {
+        "url": {
+            "description": "URL to the latest Balsamiq Mockups release.",
+        },
+        "date": {
+            "description": "Release date of the latest Balsamiq Mockups release.",
+        },
+        "version": {
+            "description": "Version of the latest Balsamiq Mockups release.",
+        },
+    }
+    description = __doc__
+
+    def get_balsamiq_url(self, base_url):
+        try:
+        	url = urllib2.urlopen(base_url).read()
+        	return json.loads( url[len('jsoncallback('):-2] )
+        	
+        except BaseException as err:
+			raise Exception("Can't read %s: %s" % (base_url, err))
+        
+    def main(self):
+        """Find and return a download URL"""
+        base_url = self.env.get("base_url", BASE_URL)
+        self.env["object"] = self.get_balsamiq_url(base_url)
+        self.env["url"] = "https://s3.amazonaws.com/build_production/mockups-desktop/Balsamiq_Mockups_"+self.env["object"]['version']+".dmg"
+        self.env["version"] = self.env["object"]['version']
+        self.env["date"] = self.env["object"]['date']
+        self.output("Found URL %s" % self.env["url"])
+
+
+if __name__ == "__main__":
+	processor = BalsamiqMockupsURLProvider()
+	processor.execute_shell()
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 autopkg
 =======
 autopkg repo-add https://github.com/autopkg/thenikola-recipes
-
-Attention:  The Balsamiq Mockups recipes are currently using static links for updates.  I realize that this is counter intuitive to the purpose of using AutoPkg.  Balsamiq no longer uses a static update link and they do not have an update stream currently.
-
-I have been updating the download link manually to avoid breaking people's autopkg automated environments, but ff they do not change this in the near future the recipes will be discontinued.
